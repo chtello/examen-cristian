f8977823018345b7b1a66fd5cb7ae3d75f531f5c
chtello <chtello@escuelasproa.edu.ar>
2024-04-29 11:04:35 -0300

    examen

---
+++ script.js
@@ -0,0 +1,27 @@
+const but1 = document.getElementsById("but1");
+const but2 = document.getElementsById("but2");
+const but3 = document.getElementsById("but3");
+const but4 = document.getElementsById("but4");
+
+const tec1 = document.getElementById("but1");
+tec1.addEventListener("click", function ()  {
+let but2
+ 
+});
+
+const tec2 = document.getElementById("but2");
+tec2.addEventListener("click", function ()  {
+ 
+});
+
+const tec3 = document.getElementById("but3");
+tec3.addEventListener("click", function ()  {
+
+ 
+});
+
+const tec4 = document.getElementById("but4");
+tec4.addEventListener("click", function ()  {
+
+ 
+});
